0fed0927643df20fa5f0f57c51167457c4b4be4a
Rafał Swacha <rafal.swacha@dreamlab.pl>
2018-09-29 17:48:36 +0200

    first screen

--- ets2-screen-arduino.ino
+++ ets2-screen-arduino.ino
@@ -10,20 +10,21 @@
 // pin 6 - LCD reset (RST)
 Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 7, 6);
 
-#define PROGRESSBAR_HEIGHT 10
+#define PROGRESSBAR_HEIGHT 5
 
 String lastMessage = "1,1";
+
+int fuelParams[] = {0,0};
+int f1 = 0;
+int f2 = 0;
 
 void setup()   {
   Serial.begin(9600);
   Serial.setTimeout(100);
+  
   display.begin();
-
   display.setContrast(60);
   display.setRotation(2);
-  
-  // text display tests
-  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(BLACK);
 }
@@ -31,31 +32,40 @@ void setup()   {
 
 void loop() {
   display.clearDisplay();
-  display.setCursor(0,0);
-  display.println(lastMessage);
-
-  int delimeterIndex = lastMessage.indexOf('/');
-  int firstVal = lastMessage.substring(0,delimeterIndex).toInt();
-  int secondVal = lastMessage.substring(delimeterIndex + 1).toInt();
-  drawProgressbar(30, firstVal, secondVal);
+  drawFuelScreen();
   display.display();
 }
 
-void drawProgressbar(int y, int value, int rangeMax) {
+void drawFuelScreen(){
+  drawScreenTitle("Paliwo");
+  display.print(fuelParams[0], DEC);
+  display.print("/");
+  display.print(fuelParams[1], DEC);
+  display.print(" litrow");
+  drawProgressbar(20, fuelParams[0], fuelParams[1]);
+}
+
+void drawScreenTitle(String title){
+  display.fillRect(0, 0, 84, 9, BLACK);
+  display.setCursor(1,1);
+  display.setTextColor(WHITE, BLACK);
+  display.print(title);
+  display.setTextColor(BLACK, WHITE);
+  display.setCursor(0,11);
+}
+
+void drawProgressbar(int yOffset, int value, int rangeMax) {
   int realValue = map(value, 0, rangeMax, 0, 82);
-  display.println(realValue);
-  display.drawRect(0, y, 84, PROGRESSBAR_HEIGHT, BLACK);
+  display.drawRect(0, yOffset, 84, PROGRESSBAR_HEIGHT, BLACK);
   if(realValue > 0) {
-    display.fillRect(1, y + 1, realValue, PROGRESSBAR_HEIGHT - 2, BLACK);
+    display.fillRect(1, yOffset + 1, realValue, PROGRESSBAR_HEIGHT - 2, BLACK);
   }
 }
 
 void serialEvent(){
   lastMessage = Serial.readString();
-}
-
-void drawFuelScreen(){
-  display.setTextColor(WHITE, BLACK);
-  display.println("Paliwo");
+  int delimeterIndex = lastMessage.indexOf(',');
+  fuelParams[0] = lastMessage.substring(0,delimeterIndex).toInt();
+  fuelParams[1] = lastMessage.substring(delimeterIndex + 1).toInt();
 }
 
